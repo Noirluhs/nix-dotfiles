56e2748a959dd5363244f6cf33df366371942f4d
Noirluhs <noirluhs@hotmail.com>
2024-08-02 19:27:31 -0400

    idc

--- emacs/default.nix
+++ emacs/default.nix
@@ -3,6 +3,13 @@
     emacs = {
       enable = true;
       package = pkgs.emacs-gtk;
+      extraConfig = "
+        (setq inhibit-startup-message t)
+	(setq visible-bell t)
+	(menu-bar-mode -1)
+	(tool-bar-mode -1)
+	(scroll-bar-mode -1)
+      ";
     };
   };
 }
