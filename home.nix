1fe828e200e37aa358fe1a810bda532186f07326
Noirluhs <noirluhs@hotmail.com>
2024-08-02 20:25:46 -0400

    idc again

--- home.nix
+++ home.nix
@@ -32,16 +32,17 @@
       shellAliases = {
         l = "tree";
         update = "sudo nixos-rebuild switch --flake .";
-	update-home = "home-manager switch --flake .";
-	cleanup = "sudo nix-collect-garbage -d";
-	gita = "git add";
-	gitc = "git commit -m";
-	gitp = "git push";
-	key = "bash ~/.key/pass/.key.sh";
+	      update-home = "home-manager switch --flake .";
+	      cleanup = "sudo nix-collect-garbage -d";
+	      gita = "git add";
+	      gitc = "git commit -m";
+	      gitp = "git push";
+        gits = "git status";
+	      key = "bash ~/.key/pass/.key.sh";
       };
       history = {
         size = 1000;
-	path = "${config.xdg.dataHome}/zsh/history";
+	      path = "${config.xdg.dataHome}/zsh/history";
       };
       oh-my-zsh = {
         enable = true;
